f950aa90598dc795e704efa0c1307bb98e9ab978
Aldyfaisal80 <98567736+Aldyfaisal80@users.noreply.github.com>
2023-12-02 17:59:53 +0700

    T13 Up

--- JS/JS1/T3/app.js
+++
@@ -1,9 +0,0 @@
-const prompt = require('prompt-sync') ()
-const { getDataMahasiswa } = require ("./index3")
-
-let counter
-
-do {
-    getDataMahasiswa (["username", "email", "nim"])
-    counter = prompt("Apakah Inggin Mengulang ? (y/n)")
-} while (counter === "y" || counter === "Y")
